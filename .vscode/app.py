b7aa90dc4fd7622716e6e212e899f7f71c763151
Nikita <nikitajha1912@gmail.com>
2023-02-04 17:35:36 -0500

    Creating the scheduler and login information

--- .vscode/app.py
+++ .vscode/app.py
@@ -6,6 +6,19 @@ app = Flask(__name__)
 # Replace YOUR_API_KEY with your Yelp API key
 API_KEY = "G0Sc2cgk4qWgBTWY4B17uUaYfaX6YbqnFBwm1KtuqQW25MzaMKPJJY50tJsybkJjnX3ZuoVsEVEK2tlsLlisA5tuV7gBFCE7rFP5_dq01VNrZZW4vgwidCFN5sjdY3Yx"
 
+@app.route("/index")
+def rank():
+    return render_template("index.html")
+
+@app.route("/schedule", methods=["POST"])
+def schedule():
+    location = request.form['location']
+    phone_number = request.form['phone_number']
+    print(location)
+    print(phone_number)
+    return render_template("schedule.html")
+
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
@@ -29,7 +42,7 @@ def index():
 
         return render_template("index.html", businesses=businesses)
 
-    return render_template("index.html")
+    return render_template("rank.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
